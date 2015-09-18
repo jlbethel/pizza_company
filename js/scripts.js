d75726ed5382ae1894cd2c44f0135ca90ae7f827
Jason Bethel <jlbethel@gmail.com>
2015-09-18 11:27:35 -0700

    write jQuery to retrieve user form information

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,28 @@ function Pizza(quantity, toppings, pizzaSize) {
 
 Pizza.prototype.calculatePrice = function() {
   var pizzaSize = this.pizzaSize;
-  var sizeCost = null;
+  var sizeCost = 0;
 
   if (pizzaSize === "small") {
-    sizeCost = 7;
+    sizeCost += 7;
   } else if (pizzaSize === "medium") {
-    sizeCost = 10;
+    sizeCost += 10;
   } else if (pizzaSize === "large") {
-    sizeCost = 13;
+    sizeCost += 13;
   }
   var finalPrice = this.quantity * (this.toppings.length + sizeCost);
   return finalPrice;
 }
+
+
+$("form#new-order").submit(function(event) {
+  event.preventDefault();
+
+  var selectedQuantity = parseInt($("input#quantity").val());
+  var selectedPizzaSize = $("#pizza-size")
+  var selectedToppings = [];
+
+  $.each($("input[name="toppings"]:checked"), function() {
+    selectedToppings.push($(this).val());
+  });
+};
